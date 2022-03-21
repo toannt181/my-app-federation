9a41ca8581ec71838c36c0dba86cc9134461eb01
Toan Nguyen Trong <toan.nt181@gmail.com>
2022-03-21 17:44:44 +0700

    Update module

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,11 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, "./src/index.tsx"),
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "index_bundle.js",
-  },
+  // entry: path.resolve(__dirname, "./src/index.tsx"),
+  // output: {
+  //   path: path.resolve(__dirname, "./dist"),
+  //   filename: "index_bundle.js",
+  // },
   devServer: {
     static: {
       directory: path.join(__dirname, "public"),
@@ -19,11 +19,11 @@ module.exports = {
   },
   plugins: [
     new container.ModuleFederationPlugin({
-      name: "host",
-      remotes: {
-        app1: "app1@http://localhost:3001/remoteEntry.js",
+      name: "MFE1",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Button": "./modules/Main/Button",
       },
-      shared: [],
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public/index.html"),
